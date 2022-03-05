4a643aa60a8a709d5199b24a086b0a9843d2e5ce
Ar Rakin <rakinar2@gmail.com>
2022-03-05 19:31:36 +0600

    Apply license comments

--- src/Entity.php
+++ src/Entity.php
@@ -1,16 +1,64 @@
 <?php
-
+/*
+ * Copyright 2020-2022 The OSN Software Foundation, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 namespace OSN\Envoy;
 
-
+/**
+ * The Entity class.
+ * An "Entity" is a wrapper of the each individual line in the env file.
+ *
+ * @package OSN\Envoy
+ * @author Ar Rakin <rakinar2@gmail.com>
+ */
 class Entity
 {
+    /**
+     * The raw line.
+     *
+     * @var string
+     */
     protected string $raw;
+
+    /**
+     * The field name.
+     *
+     * @var string
+     */
     protected string $field;
+
+    /**
+     * The field value.
+     *
+     * @var string|null
+     */
     protected ?string $value;
+
+    /**
+     * Line count.
+     *
+     * @var int
+     */
     public static int $line = 0;
 
+    /**
+     * Entity constructor.
+     *
+     * @param string $line
+     */
     public function __construct(string $line)
     {
         $this->raw = $line;
@@ -19,6 +67,8 @@ class Entity
     }
 
     /**
+     * Get the raw line.
+     *
      * @return string
      */
     public function raw(): string
@@ -26,6 +76,11 @@ class Entity
         return $this->raw;
     }
 
+    /**
+     * Parse the raw line.
+     *
+     * @return void
+     */
     public function parse()
     {
         $equalPos = strpos($this->raw, '=');
@@ -49,6 +104,12 @@ class Entity
         $this->value = $value === null ? null : $this->removeQuotes(trim($value));
     }
 
+    /**
+     * Remove extra quotes from a string start and end.
+     *
+     * @param $value
+     * @return false|mixed|string
+     */
     protected function removeQuotes($value)
     {
         if (($value[0] === '"' && $value[-1] === '"') || ($value[0] === "'" && $value[-1] === "'")) {
@@ -59,6 +120,8 @@ class Entity
     }
 
     /**
+     * Get the field name.
+     *
      * @return string
      */
     public function field(): string
@@ -67,7 +130,9 @@ class Entity
     }
 
     /**
-     * @return string
+     * Get the field value.
+     *
+     * @return string|null
      */
     public function value(): ?string
     {
--- src/Envoy.php
+++ src/Envoy.php
@@ -1,9 +1,24 @@
 <?php
+/*
+ * Copyright 2020-2022 The OSN Software Foundation, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 namespace OSN\Envoy;
 
 /**
- * Class Envoy
+ * Class Envoy, the main class.
  *
  * @package OSN\Envoy
  * @author Ar Rakin <rakinar2@gmail.com>
@@ -12,19 +27,36 @@ class Envoy
 {
     public const CONFIG_ASSIGN_ENV_TO_SERVER = 1;
 
+    /**
+     * The full env file path.
+     *
+     * @var string
+     */
     protected string $envFile;
+
+    /**
+     * Envoy-related configuration storage.
+     *
+     * @var array
+     */
     protected array $config;
 
+    /**
+     * Envoy constructor.
+     *
+     * @param string|null $envFile
+     * @param array $config
+     */
     public function __construct(?string $envFile = null, array $config = [])
     {
         $this->envFile = $envFile ?? './.env';
-
         $config[static::CONFIG_ASSIGN_ENV_TO_SERVER] = $config[static::CONFIG_ASSIGN_ENV_TO_SERVER] ?? false;
-
         $this->config = $config;
     }
 
     /**
+     * Parse the env file and return an array of Entities.
+     *
      * @return Entity[]
      */
     public function parse(): array
@@ -44,7 +76,12 @@ class Envoy
         return $entities;
     }
 
-    public function load()
+    /**
+     * Parse the env file and then load all data to the super-global variables.
+     *
+     * @return void
+     */
+    public function load(): void
     {
         $entities = $this->parse();
 
@@ -57,6 +94,12 @@ class Envoy
         }
     }
 
+    /**
+     * A safe wrapper for getting environment configuration values.
+     *
+     * @param mixed $env
+     * @return mixed|null
+     */
     public static function getENV($env)
     {
         return $_ENV[$env] ?? null;
